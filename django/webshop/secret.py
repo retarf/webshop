ba12bc6a0708a8814e6dd1e3f6e05d8e146040a7
retarf <retarf@gmail.com>
2019-12-12 17:32:56 +0100

    Postgresql database settings added.

---
+++ django/webshop/secret.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'webshop',
+        'USER': 'admin',
+        'PASSWORD': 'mypass',
+        'HOST': 'db',
+        'PORT': '5432',
+        'PORT': '',
+    }
+}
