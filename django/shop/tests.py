1337e2567f3b37573ae17fa351d7918443a38003
retarf <retarf@gmail.com>
2019-12-12 21:31:55 +0100

    Product tests added

--- django/shop/tests.py
+++ django/shop/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Product
 
-# Create your tests here.
+class ProductTestCase(TestCase):
+
+    def setUp(self):
+        Product.objects.create(
+                name='TestProduct1',
+                description='Description TestProduct1'
+                )
+        Product.objects.create(
+                name='TestProduct2',
+                description='Description TestProduct2'
+                )
+
+    def test_product_description(self):
+        product1 = Product.objects.get(name='TestProduct1')
+        product2 = Product.objects.get(name='TestProduct2')
+        self.assertEqual(product1.description, 'description TestProduct1')
+        self.assertEqual(product2.description, 'description TestProduct2')
+                
