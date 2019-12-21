810a58492532fa3d879002308d6057309d681afe
retarf <retarf@gmail.com>
2019-12-21 23:30:43 +0100

    Product detail view added

--- django/shop/models.py
+++ django/shop/models.py
@@ -8,3 +8,15 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Item(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return self.product.name
+
+    def get_value(self):
+        return quantity * price
+
--- django/shop/urls.py
+++ django/shop/urls.py
@@ -5,5 +5,6 @@ from . import views
 namespace = 'shop'
 
 urlpatterns = [
-        path('products', views.ProductListView.as_view(), name='products'),
+        path('products/', views.ProductListView.as_view(), name='products'),
+        path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product'),
         ]
